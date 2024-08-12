bd9204bc0d9cd96c830111bb314db968b8e32e79
jacpa42 <jpctenthoven@gmail.com>
2024-08-13 00:20:01 +0200

    Removed stupid function

--- fish/.config/fish/functions/update.fish
+++
@@ -1,4 +0,0 @@
-function update --wraps=./home/jacob/AUR/update.sh --wraps=/home/jacob/AUR/update.sh --description 'alias update=/home/jacob/AUR/update.sh'
-  /home/jacob/AUR/update.sh $argv
-        
-end
