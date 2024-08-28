cdd650c2ea38663067dd130fd608758309fdbf31
jacpa42 <jpctenthoven@gmail.com>
2024-08-28 09:26:33 +0200

    bruh

--- fish/functions/l.fish
+++ fish/functions/l.fish
@@ -1,4 +1,4 @@
-function l --wraps='eza -L --git -T --header' --wraps='eza -l --git -T --header' --wraps='eza --git -T --header' --wraps='eza --git --header' --wraps='eza -l --git --header' --description 'alias l eza -l --git --header'
-  eza -l --git --header $argv
+function l --wraps='eza -L --git -T --header' --wraps='eza -l --git -T --header' --wraps='eza --git -T --header' --wraps='eza --git --header' --wraps='eza -l --git --header' --wraps='eza --long --no-time --no-user --header --colour always --icons' --description 'alias l=eza --long --no-time --no-user --header --colour always --icons'
+  eza --long --no-time --no-user --header --colour always --icons $argv
         
 end
---
+++ fish/functions/lll.fish
@@ -0,0 +1,4 @@
+function lll --wraps='eza --total-size --long --colour always --icons --git --all' --description 'alias lll=eza --total-size --long --colour always --icons --git --all'
+  eza --total-size --long --colour always --icons --git --all $argv
+        
+end
