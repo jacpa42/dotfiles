a95fe5fd5a91a0011ab7221b2f21ad854fd5016c
jacpa42 <jpctenthoven@gmail.com>
2024-09-13 07:53:07 +0200

    btuh

--- fish/functions/update.fish
+++ fish/functions/update.fish
@@ -1,4 +1,4 @@
 function update --description 'alias update=sudo pacman -Syu && echo && yay && echo && rustup update'
-  sudo pacman -Syu && echo && yay && echo && rustup update $argv
-        
+    sudo pacman -Syu && echo && yay && echo && rustup update $argv
+
 end
