f55b903eb41f21be1997608cd5db51abf525efc6
jacpa42 <jacobenthoven@gmail.com>
2025-01-04 12:15:40 +0200

    added new neovim conf

--- nvim_bak/lua/config/keymaps.lua
+++
@@ -1,9 +0,0 @@
--- Keymaps are automatically loaded on the VeryLazy event
-
-local map = vim.keymap.set
-
-map("n", "<C-f>", "<cmd>on<cr>", { desc = "Focus current window" })
-
-map("n", "<leader>ch", function()
-  vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
-end, { desc = "Toggle inlay hints" })
