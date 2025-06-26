f9e546b03c112c92fac589a24b766396951eba9b
jacpa42 <jacobenthoven@gmail.com>
2025-06-26 12:52:37 +0200

    update wofi script

--- hypr/wofi.sh
+++ hypr/wofi.sh
@@ -3,12 +3,6 @@
 CONFIG="$XDG_CONFIG_HOME/hypr/wofi/config/config"
 STYLE="$XDG_CONFIG_HOME/hypr/wofi/src/mocha/style.css"
 
-if [[ ! $(pidof wofi) ]]; then
-	if [ -t 0 ]; then
-		wofi --monitor="DP-1" --conf "${CONFIG}" --style "${STYLE}" "$@"
-	else
-		wofi --monitor="DP-1" --conf "${CONFIG}" --style "${STYLE}" "$@" <&0
-	fi
-else
-  pkill wofi
-fi
+pidof wofi >/dev/null && pkill wofi && exit
+
+wofi --monitor="DP-1" --conf "${CONFIG}" --style "${STYLE}" "$@" <&0
