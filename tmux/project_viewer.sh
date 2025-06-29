c00d65fc3b5ddb0c81757fcc79ae8d51e13856cf
jacpa42 <jacobenthoven@gmail.com>
2025-06-29 14:37:52 +0200

    bug fixes with tmux.conf

--- tmux/project_viewer.sh
+++ tmux/project_viewer.sh
@@ -2,11 +2,17 @@
 
 [ -z "$1" ] && exit 1
 
-local dir="$1"
-local name=$(basename "$dir")
+local name=$(basename "$1")
 
-if tmux has-session -t "$name" 2>/dev/null; then
-	tmux capture-pane -et "$name" -p
-else
-	eza --color=always --sort=type --long --icons always --no-time --no-user --header "$dir"
-fi
+case "$(xdg-mime query filetype "$1")" in
+inode/directory)
+	if tmux has-session -t "$name" 2>/dev/null; then
+		tmux capture-pane -et "$name" -p
+	else
+		eza --color=always --sort=type --long --icons always --no-time --no-user --header "$1"
+	fi
+	;;
+*)
+	bat "$1"
+	;;
+esac
--- tmux/projects.sh
+++ tmux/projects.sh
@@ -2,17 +2,15 @@
 
 LAZYGIT=0
 
-export -f preview
-
 case "$1" in
-	git)
-		# Opens lazy git here in new pane
-		LAZYGIT=1
-		TARGET_DIR="$(echo -e "$(fd --exec="dirname" -Htd --glob .git "$HOME/.config")\n$(fd --exec="dirname" -Htd --glob .git "$HOME/Projects")" | sk --preview="eza --color=always --sort=type --long --icons always --no-time --no-user --header {}")"
-		;;
-	*)
-		TARGET_DIR="$(echo -e "$HOME\n$(fd --exec="dirname" -Htd --glob .git "$HOME/.config")\n$(fd --exec="dirname" -Htd --glob .git "$HOME/Projects")" | sk --preview-window="right:60%" --preview="$HOME/.config/tmux/project_viewer.sh {}")"
-		;;
+git)
+	# Opens lazy git here in new pane
+	LAZYGIT=1
+	TARGET_DIR="$(pwd)"
+	;;
+*)
+	TARGET_DIR="$(echo -e "$HOME\n$(fd --exec="dirname" -Htd --glob .git "$HOME/.config")\n$(fd --exec="dirname" -Htd --glob .git "$HOME/Projects")" | sk --preview-window="right:60%" --preview="$HOME/.config/tmux/project_viewer.sh {}")"
+	;;
 esac
 
 [ -z "$TARGET_DIR" ] && exit 1
@@ -21,24 +19,21 @@ SESSION_NAME=$(basename "$TARGET_DIR")
 # If we want lazygit:
 if [ $LAZYGIT -eq 1 ]; then
 	cd "$TARGET_DIR"
-	tmux display-popup -E -w 30% -h 40% -T "$SESSION_NAME" "lazgit" 2>$HOME/log.txt
-	disown
-	exit 0
+	exec lazygit
 fi
 
 # Otherwise just switch tmux sessions
 FOUND_SESSION=$(tmux list-sessions -F "#{session_name}" 2>/dev/null | while read -r s; do
-  dir=$(tmux display-message -p -t "$s" "#{session_path}")
-  if [ "$dir" = "$TARGET_DIR" ]; then
-    echo "$s"
-    break
-  fi
+	dir=$(tmux display-message -p -t "$s" "#{session_path}")
+	if [ "$dir" = "$TARGET_DIR" ]; then
+		echo "$s"
+		break
+	fi
 done)
 
 if [ -n "$FOUND_SESSION" ]; then
-  tmux switch-client -t "$FOUND_SESSION"
+	tmux switch-client -t "$FOUND_SESSION"
 else
-  tmux new-session -ds "$SESSION_NAME" -c "$TARGET_DIR"
-  tmux switch-client -t "$SESSION_NAME"
+	tmux new-session -ds "$SESSION_NAME" -c "$TARGET_DIR"
+	tmux switch-client -t "$SESSION_NAME"
 fi
-
