1bb5c295d2de8a857e8fdf413acdd31c8fa8aaab
jacpa42 <jacobenthoven@gmail.com>
2025-06-29 11:07:58 +0200

    Trying to get popups to be useful in tmux

--- tmux/projects.sh
+++ tmux/projects.sh
@@ -1,2 +1,6 @@
 # TODO:	Make this actually useful
-fd -td --base-directory="$HOME" | sk
+# Ideas: Wallpaper switcher with terminal image viewer and Tmux session fuzzy finder
+# I only use yazi for the image viewing and the zoxide integration. This can be
+# done with fd, yazi? and tmux!
+
+fd -aejpg -tfile | fzf --preview="env icat {}"
