2ae634396a6315d98df5c9e30e7b64167ef031bc
jacpa42 <jacobenthoven@gmail.com>
2025-08-05 21:14:55 +0200

    added back noice nvim

---
+++ nvim/lua/plugins/noice.lua
@@ -0,0 +1,20 @@
+return {
+	"folke/noice.nvim",
+	event = "VeryLazy",
+	opts = {
+		timeout = 1000,
+		lsp = {
+			override = {
+				["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+				["vim.lsp.util.stylize_markdown"] = true,
+			},
+		},
+		presets = {
+			bottom_search = false,
+			command_palette = true, -- position the cmdline and popupmenu together
+			long_message_to_split = true, -- long messages will be sent to a split
+			inc_rename = false, -- enables an input dialog for inc-rename.nvim
+			lsp_doc_border = true, -- add a border to hover docs and signature help
+		},
+	},
+}
