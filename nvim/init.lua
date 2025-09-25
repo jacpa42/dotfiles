36843a8b72f01d152c49998fa535e055c613dd43
jacpa42 <jacobenthoven@gmail.com>
2025-09-25 18:01:28 +0200

    start tree sitter on c attach

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,7 +1,6 @@
 ---------------------- LAZY ----------------------
--- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv).fs_stat(lazypath) then
+if not vim.uv.fs_stat(lazypath) then
 	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
 	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
 	if vim.v.shell_error ~= 0 then
@@ -518,6 +517,13 @@ vim.api.nvim_create_autocmd("TermOpen", {
 		vim.opt.number = false
 		vim.opt.relativenumber = false
 		vim.cmd.norm("i")
+	end,
+})
+
+vim.api.nvim_create_autocmd("BufAdd", {
+	pattern = { "*.c" },
+	callback = function(args)
+		vim.treesitter.start(args.buf, "c")
 	end,
 })
 
