abcfdaba169e1c222776fd408c03d153d38f22c7
jacpa42 <jacobenthoven@gmail.com>
2026-01-04 21:15:59 +0200

    removed debugging code

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -19,25 +19,6 @@ vim.api.nvim_create_autocmd("LspAttach", {
 	end,
 })
 
--- Source - https://stackoverflow.com/a
--- Posted by hookenz, modified by community. See post 'Timeline' for change history
--- Retrieved 2026-01-04, License - CC BY-SA 4.0
-
-function dump(o)
-	if type(o) == "table" then
-		local s = "{ "
-		for k, v in pairs(o) do
-			if type(k) ~= "number" then
-				k = '"' .. k .. '"'
-			end
-			s = s .. "[" .. k .. "] = " .. dump(v) .. ","
-		end
-		return s .. "} "
-	else
-		return tostring(o)
-	end
-end
-
 vim.api.nvim_create_autocmd("BufWritePre", {
 	pattern = "*",
 	callback = function(args)
