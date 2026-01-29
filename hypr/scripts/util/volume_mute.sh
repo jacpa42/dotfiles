d0b362c907e268e64599779046002f9680c0ac7d
jacpa42 <jacobenthoven@gmail.com>
2026-01-29 08:00:26 +0100

    proper mute

--- hypr/scripts/util/volume_mute.sh
+++ hypr/scripts/util/volume_mute.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env /usr/bin/sh
 
-wpctl set-volume @DEFAULT_AUDIO_SINK@ 0% -l 1.0
+wpctl set-mute @DEFAULT_SINK@ toggle
 msg="volume set to $(wpctl get-volume @DEFAULT_AUDIO_SINK@)"
 notify-send -t 1000 -c media -r 777 "$msg"
