7facd0a346499e28957ccf8b7c81772f8d0a652b
Jacob Enthoven <jacob.enthoven@kragsentrale.co.za>
2026-02-04 18:52:58 +0200

    custom firefox chooser

--- linkshit.sh
+++ linkshit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DOTDIR="${DOTDIR:-$HOME/projects/dotfiles}"
-CNFDIR="$HOME/.config"
+CNFDIR="${XDG_CONFIG_HOME:-$HOME/.config}"
 
 ln -vsfn $DOTDIR/ashell $CNFDIR/ashell
 ln -vsfn $DOTDIR/bat $CNFDIR/bat
@@ -17,3 +17,5 @@ ln -vsfn $DOTDIR/zsh/.zprofile $HOME/.zprofile
 ln -vsfn $DOTDIR/lazygit $HOME/lazygit
 ln -vsfn $DOTDIR/foot $CNFDIR/foot
 ln -vsfn $DOTDIR/qutebrowser $CNFDIR/qutebrowser
+ln -vsfn $DOTDIR/xdg-desktop-portal-termfilechooser $CNFDIR/xdg-desktop-portal-termfilechooser
+ln -vsfn $DOTDIR/xdg-desktop-portal $CNFDIR/xdg-desktop-portal
