d8543ce0fcbe7c70113633ff7287a1a5322219bd
Jacob Enthoven <jacob.enthoven@kragsentrale.co.za>
2026-02-06 18:22:41 +0200

    sort by last_visit_date rather than frecency

--- hypr/scripts/pickers/firefox_history.sh
+++ hypr/scripts/pickers/firefox_history.sh
@@ -6,7 +6,7 @@ trap 'rm "$backup_copy"' EXIT
 
 sqlite3 "file:$file?mode=ro&immutable=1" ".backup $backup_copy"
 
-query="select title,url from moz_places where title is not null order by frecency desc"
+query="select title,url from moz_places where title is not null order by last_visit_date desc"
 sep=".separator \" \""
 
 selection="$(sqlite3 "$backup_copy" "$sep" "$query" | fzf --bind 'ctrl-s:execute-silent(hyprctl dispatch exec xdg-open {-1})' --ghost="search history" --header="ctrl-s to open in background")"
