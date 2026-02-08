05ec688c7e141da3fb607d5abcabb995d644dbda
Jacob Enthoven <jacob.enthoven@kragsentrale.co.za>
2026-02-08 16:14:32 +0200

    1% instead of 2% for volume

--- hypr/scripts/util/volume_increase.sh
+++ hypr/scripts/util/volume_increase.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env /usr/bin/sh
 
-wpctl set-volume @DEFAULT_AUDIO_SINK@ 2%+ -l 1.0
+wpctl set-volume @DEFAULT_AUDIO_SINK@ 1%+ -l 1.0
 msg="volume set to $(wpctl get-volume @DEFAULT_AUDIO_SINK@)"
 notify-send -t 1000 -c media -r 777 "$msg"
