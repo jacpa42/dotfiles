ae1fb90eaa5de354616fcbc653b32ace088035cd
jacpa42 <jacob.enthoven@kragsentrale.co.za>
2026-02-21 10:43:06 +0200

    upgraded the openfile script. Install script was buggy af

--- hypr/scripts/util/openfile.sh
+++ hypr/scripts/util/openfile.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env /usr/bin/sh
-
-xdg-open "$(fd -atfile . $HOME | fzf)"
+selected="$(fzf --walker-root=$HOME --walker=file \
+    --bind 'ctrl-s:execute-silent(hyprctl dispatch exec xdg-open {-1})' \
+    --ghost="open file" \
+    --header="ctrl-s to open in background")"
+hyprctl dispatch exec "xdg-open \""$selected"\""
