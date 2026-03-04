c12273ceaa14316aa20a9bb6d8716b5de3acaf0d
Jacob Enthoven <jacob.enthoven@kragsentrale.co.za>
2026-03-04 08:23:02 +0200

    fixed websearch

--- dotconfig/hypr/scripts/util/webseach.sh
+++ dotconfig/hypr/scripts/util/webseach.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-output="$(fuzzel -l1 --dmenu --placeholder="search web" <<<"")"
+output="$(fuzzel -l0 --dmenu --placeholder="search web")"
 [ -z "$output" ] && exit 0
 url="https://www.google.com/search?q=$(printf "%s" "$output" | sed -n '1p' | urlencode)&udm=14"
 hyprctl dispatch workspace 3
