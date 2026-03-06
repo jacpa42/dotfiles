5a3d7806d1584c27a6d44099e309201fa15a12cd
jacpa42 <jacob.enthoven@kragsentrale.co.za>
2026-03-06 21:51:33 +0200

    added some stuff

--- dotconfig/hypr/scripts/util/openfile.sh
+++ dotconfig/hypr/scripts/util/openfile.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-selected="$(fd -tf . $HOME | fuzzel --dmenu --placeholder="open file")"
+selected="$(fd -utf -E '\.*/' . $HOME | fuzzel --dmenu --placeholder="open file")"
 exec xdg-open $selected
