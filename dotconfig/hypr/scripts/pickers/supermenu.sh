b6a1aee0baae42100be45993ffee7cab353887dd
jacpa42 <jacob.enthoven@kragsentrale.co.za>
2026-03-07 18:14:23 +0200

    auto select on super menu

--- dotconfig/hypr/scripts/pickers/supermenu.sh
+++ dotconfig/hypr/scripts/pickers/supermenu.sh
@@ -5,7 +5,7 @@ cd $script_dir || exit
 
 preview='bat --plain --color=always --language=sh $(fd --min-depth 2 --glob -1 -texecutable {1}\*)'
 
-selected="$(fd --min-depth 2 -texecutable --format "{/.}" | fuzzel --dmenu)"
+selected="$(fd --min-depth 2 -texecutable --format "{/.}" | fuzzel --dmenu --auto-select)"
 [ -z "$selected" ] && exit
 
 selected=$(fd --min-depth 2 --glob -1 -texecutable "$selected*")
