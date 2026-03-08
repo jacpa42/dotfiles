f2f6e4bcf98ca70120ede8ca439fc151ea144387
Jacob Enthoven <jacob.enthoven@kragsentrale.co.za>
2026-03-08 11:15:31 +0200

    kinda works lmao
    I can open terminal apps now :)

--- dotconfig/hypr/scripts/util/openfile.sh
+++ dotconfig/hypr/scripts/util/openfile.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
 
 selected="$(fd -utf -E '\.*/' . $HOME | fuzzel --dmenu --placeholder="open file")"
-exec xdg-open $selected
+
+mime_type="$(xdg-mime query filetype "$selected")"
+app="$(xdg-mime query default "$mime_type")"
+desktop_file="$(fd -1 "$app" /usr/share/applications ~/.local/share/applications/)"
+
+grep -q "^Terminal=true$" "$desktop_file" && {
+    app_name="$(grep -m1 -oP 'Icon=\K.*' "$desktop_file")"
+    footclient -a "$app_name" -T "$app_name" xdg-open "$selected"
+} || {
+    xdg-open "$selected"
+}
