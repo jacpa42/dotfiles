c2db6018330c4c6c9cbbbfae837ea101245d5cf7
Jacob Enthoven <jacob.enthoven@kragsentrale.co.za>
2026-03-18 20:42:21 +0200

    cleaned up tmuxer script

--- dotconfig/hypr/scripts/util/tmuxer.sh
+++ dotconfig/hypr/scripts/util/tmuxer.sh
@@ -1,19 +1,22 @@
 #!/usr/bin/env bash
 
 TARGET_DIR=""
-SESSION_NAME=""
 OPERATION=""
 WORKING_DIRECTORY="$(tmux display-message -Cp -d 0 '#{session_path}' || exit 0)"
 cd "$WORKING_DIRECTORY"
 
-focus_terminal_window() {
+focus_or_create_tmux_window() {
     local window_class="tmux"
     local switch_window="$(hyprctl dispatch focuswindow "class:$window_class")"
-    [[ "$switch_window" == "ok" ]] || {
-        notify-send --expire-time=3000 --replace-id=21346532 "Failed to find window with class:\"$window_class\""
-        return 1
-    }
+    [[ "$switch_window" == "ok" ]] || gtk-launch tmux
     return 0
+}
+
+switch_to_project() {
+    local target_dir="$1"
+    local session_name="$(basename $target_dir)"
+    tmux new-session -d -s "$session_name" -c "$target_dir" -n "code"
+    tmux switch-client -t "$session_name"
 }
 
 case "$1" in
@@ -41,55 +44,18 @@ btop)
     exec btop
     ;;
 projects)
-    WORKING_DIRECTORY="$PROJDIR"
-    cd "$WORKING_DIRECTORY"
+    cd "$PROJDIR"
 
     TARGET_DIR="$(fd --format="{//}" -Hgtd .git | fuzzel --dmenu --placeholder="Choose project")"
     [[ -z "$TARGET_DIR" ]] && exit 0 || TARGET_DIR="$PROJDIR/$TARGET_DIR"
-    [[ -d "$TARGET_DIR" ]] || {
-        notify-send -t 2000 "\"$TARGET_DIR\" is not a directory :("
-        exit 1
-    }
+    [[ -d "$TARGET_DIR" ]] || exit 1
+    focus_or_create_tmux_window || exit 1
 
-    focus_terminal_window || exit 1
-
-    SESSION_NAME=$(basename "$TARGET_DIR")
-
-    # Otherwise just switch tmux sessions
-    FOUND_SESSION=$(tmux list-sessions -F "#{session_name}" 2>/dev/null | while read -r s; do
-        dir=$(tmux display-message -p -t "$s" "#{session_path}")
-        if [ "$dir" = "$TARGET_DIR" ]; then
-            echo "$s"
-            break
-        fi
-    done)
-
-    if [[ -n "$FOUND_SESSION" ]]; then
-        tmux switch-client -t "$FOUND_SESSION"
-    else
-        tmux new-session -dAs "$SESSION_NAME" -c "$TARGET_DIR" -n "code" 'exec /usr/bin/env $SHELL'
-        tmux switch-client -t "$SESSION_NAME"
-    fi
+    switch_to_project "$TARGET_DIR"
     ;;
 home_session)
     TARGET_DIR="$HOME"
-    SESSION_NAME=$(basename "$TARGET_DIR")
-    focus_terminal_window || exit 1
-
-    # Otherwise just switch tmux sessions
-    FOUND_SESSION=$(tmux list-sessions -F "#{session_name}" 2>/dev/null | while read -r s; do
-        dir=$(tmux display-message -p -t "$s" "#{session_path}")
-        if [ "$dir" = "$TARGET_DIR" ]; then
-            echo "$s"
-            break
-        fi
-    done)
-
-    if [ -n "$FOUND_SESSION" ]; then
-        tmux switch-client -t "$FOUND_SESSION"
-    else
-        tmux new-session -dAs "$SESSION_NAME" -c "$TARGET_DIR" -n "code" 'exec /usr/bin/env $SHELL'
-        tmux switch-client -t "$SESSION_NAME"
-    fi
+    focus_or_create_tmux_window || exit 1
+    switch_to_project "$TARGET_DIR"
     ;;
 esac
