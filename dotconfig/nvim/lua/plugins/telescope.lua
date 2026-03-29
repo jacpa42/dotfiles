95de223bce2b5af8d03b5eb83e6699f2abf76b49
jacpa42 <jacob.enthoven@kragsentrale.co.za>
2026-03-29 10:53:11 +0200

    fixed focus window
    When there are 2 windows with the same class and we have focused one of
    them already, dont try to focus the same class

--- dotconfig/nvim/lua/plugins/telescope.lua
+++ dotconfig/nvim/lua/plugins/telescope.lua
@@ -1,6 +1,8 @@
 return {
 	"nvim-telescope/telescope.nvim",
+	version = "*",
 	dependencies = {
+		"nvim-lua/plenary.nvim",
 		"nvim-tree/nvim-web-devicons",
 		"nvim-telescope/telescope-ui-select.nvim",
 		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
