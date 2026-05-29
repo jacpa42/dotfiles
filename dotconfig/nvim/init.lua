b9c51051c3515870c4357459a4ef4bf7cc26df6d
jacpa42 <jacob.enthoven@kragsentrale.co.za>
2026-05-29 10:48:11 +0200

    fixed some bugs

--- dotconfig/nvim/init.lua
+++ dotconfig/nvim/init.lua
@@ -160,6 +160,13 @@ autocmd({ "FileType" }, {
 	end,
 })
 
+autocmd({ "FileType" }, {
+	pattern = "odin",
+	callback = function()
+		vim.opt.errorformat = { "%f(%l:%c) %m" }
+	end,
+})
+
 vim.cmd("autocmd TextYankPost * silent! lua vim.hl.on_yank {higroup='Visual', timeout=100}")
 
 ----------------------------------lsp----------------------------------
